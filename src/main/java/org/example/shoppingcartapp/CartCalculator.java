c3e00068d2722ad1bad647f02106fff72ca66cf4
Melke-At <melkamuy@metropolia.fi>
2026-03-29 23:52:24 +0300

    Initial commit

---
+++ src/main/java/org/example/shoppingcartapp/CartCalculator.java
@@ -0,0 +1,4 @@
+package org.example.shoppingcartapp;
+
+public class CartCalculator {
+}
---
+++ src/src/test/java/org/example/shoppingcartapp/CartCalculatorTest.java
@@ -0,0 +1,22 @@
+import org.example.CartService;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CartServiceTest {
+
+    CartService service = new CartService();
+
+    @Test
+    void testItemTotal() {
+        assertEquals(20.0, service.calculateItemTotal(10, 2));
+    }
+
+    @Test
+    void testCartTotal() {
+        List<Double> items = List.of(10.0, 20.0, 30.0);
+        assertEquals(60.0, service.calculateCartTotal(items));
+    }
+}
